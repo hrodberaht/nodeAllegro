7f81121012e1142c0b2884dc63b4d6f1fbaf38d4
hrodberaht <robpszczolka@gmail.com>
2018-08-15 21:29:23 +0200

    refactoring

--- index.js
+++ index.js
@@ -4,18 +4,16 @@ const soap = require("soap");
 const config = require("./config");
 const app = new Koa();
  
-const url = config.url;
-const webapiKey = config.webapiKey;
+const {url,webapiKey} = config;
 const args = {
     countryId: 1,
     webapiKey
 } 
 
 app.use(async ctx => {
-
-        const client = await soap.createClientAsync(url);
-        const result = await client.doQueryAllSysStatusAsync({countryId: '1',webapiKey: 'e2dcc5cd'});
-        ctx.body = result[0].sysCountryStatus;
+    const client = await soap.createClientAsync(url);
+    const result = await client.doQueryAllSysStatusAsync(args);
+    ctx.body = result[0].sysCountryStatus;
 })
 
 app.listen(3000, () => {
