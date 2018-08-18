1c41bf655f8edb3d2074ff0f7721a78cb8bfa7cc
hrodberaht <robpszczolka@gmail.com>
2018-08-18 19:37:26 +0200

    add hash password and login to allegro

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
 const Koa = require("koa");
 const soap = require("soap");
+const crypto = require("crypto");
 
 const config = require("./config");
 const app = new Koa();
+
+
  
-const {url,webapiKey} = config;
+const {url,webapiKey,userLogin,password,secret} = config;
+
+const hashPassword = crypto.createHash('sha256')
+                            .update(password)
+                            .digest('base64');
 const args = {
     countryId: 1,
-    webapiKey
+    webapiKey,
 } 
+
+console.log(hashPassword);
 
 app.use(async ctx => {
     const client = await soap.createClientAsync(url);
     const result = await client.doQueryAllSysStatusAsync(args);
-    ctx.body = result[0].sysCountryStatus;
+    const verKey = result[0].sysCountryStatus.item[0].verKey;
+
+    const login = await client.doLoginEncAsync({
+        userLogin: userLogin,
+        userHashPassword: hashPassword,
+        countryCode: 1,
+        webapiKey,
+        localVersion: verKey
+
+    })
+    console.log(login);
 })
 
 app.listen(3000, () => {
